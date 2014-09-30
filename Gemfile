c29dbc9ddf149634cec81e53aba8f071d60f777d
Evaldas Grudzinskas <evaldas.g@icloud.com>
2014-09-30 22:41:11 +0300

    Add count_bonuses to application controller, update index accordingly.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
-
 gem 'pg'
 gem 'thin'
 
@@ -20,7 +19,10 @@ gem 'autoprefixer-rails'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
-gem 'spring',        group: :development
+group :development do
+  gem 'spring'
+  gem 'pry'
+end
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,37 @@ class ApplicationController < ActionController::Base
 
   def index
   end
+
+  def bonus_counter
+    count_bonuses(params[:id])
+    render 'index'
+  end
+
+  private
+  def count_bonuses(user_id)
+    @bonus_amount = 0
+    # creating GET request for json
+    uri = URI.parse("http://sheltered-refuge-8118.herokuapp.com/all.json")
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.request_uri)
+    res = http.request(req)
+
+    if res.code == '200' # using response only if it was success
+      results = JSON.parse(res.body) # parsing json to ruby
+
+      # selecting all user's bonuses fragments and inserting into new array
+      user_results = results.select { |r| r["user_id"] == user_id.to_i }
+      array = []
+      user_results.each do |result|
+        result["fragments"].each do |fragment|
+          if fragment["rel_type"] == 'b'
+            fragment["date"] = result["date"] # this attribute is needed for fragment also
+            array << fragment
+          end
+        end
+      end
+      array.each { |a| @bonus_amount += a["amount"] } # counting bonus amount
+      @bonus = array.sort { |a, b| a["rel_id"] <=> b["rel_id"] }
+    end
+  end
 end
